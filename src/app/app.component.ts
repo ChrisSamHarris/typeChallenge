a93ff9ef0df9d874cb693a0b1529daf2bbfd14ca
chharris <chharris@chharriss-MacBook-Pro.local>
2022-01-10 16:43:20 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { lorem } from "faker";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'type';
+  randomText = lorem.sentence();
+  enteredText = '';
+  //randomText<property> = componentOfFaker.capability()
+
+  onInput(value: string) {
+    console.log(value);
+    this.enteredText = value;
+  }
 }
